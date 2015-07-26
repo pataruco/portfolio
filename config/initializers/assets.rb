60d60c12288db307c4666693f4970a17b595d7ae
Pedro Martin <pataruco@gmail.com>
2015-07-26 13:24:51 +0100

    Font Sketchbook added

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,10 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 
+#Enable the asset pipeline
+Rails.application.config.assets.enabled = true
+Rails.application.config.assets.paths << Rails.root.join('/app/assets/fonts')
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
